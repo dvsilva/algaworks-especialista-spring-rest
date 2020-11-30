feee6d5c2ee32b2b9059f90f33945c6dd166b225
dvsilva <dvsilva@gmail.com>
2020-11-30 11:23:38 -0300

    Acessando propriedades com @ConfigurationProperties

--- algafood-api/src/main/java/com/algaworks/algafood/listener/NotificacaoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/listener/NotificacaoService.java
@@ -1,12 +1,12 @@
 package com.algaworks.algafood.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.NotificadorProperties;
 import com.algaworks.algafood.di.notificacao.TipoNotificador;
 import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
 
@@ -17,16 +17,22 @@ public class NotificacaoService {
 	@Autowired
 	private Notificador notificador;
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
+	@Autowired
+	private NotificadorProperties properties;
+	
+//	@Value("${notificador.email.host-servidor}")
+//	private String host;
 
-	@Value("${notificador.email.porta-servidor}")
-	private String port;
+//	@Value("${notificador.email.porta-servidor}")
+//	private String port;
 	
 	@EventListener
 	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
-		System.out.println("Host: " + host);
-		System.out.println("Porta: " + port);
+//		System.out.println("Host: " + host);
+//		System.out.println("Porta: " + port);
+		
+		System.out.println("Host: " + properties.getHostServidor());
+		System.out.println("Porta: " + properties.getPortServidor());
 		
 		System.out.println("Cliente " + event.getCliente().getNome() + " agora está ativo");
 		this.notificador.notificar(event.getCliente(), "Seu cadastro no sistema está ativo!");
