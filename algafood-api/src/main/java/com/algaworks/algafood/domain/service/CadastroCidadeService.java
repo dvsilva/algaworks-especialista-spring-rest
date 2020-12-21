b214201aa9b335be1ac7481de712e68ae8802efb
dvsilva <dvsilva@gmail.com>
2020-12-21 07:53:24 -0300

    Analisando os impactos da refatoração

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -10,28 +10,22 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
-import com.algaworks.algafood.domain.repository.EstadoRepository;
 
 @Service
 public class CadastroCidadeService {
 
 	private static final String MSG_CIDADE_EM_USO = "Cidade de código %d não pode ser removida pois está em uso";
 	private static final String MSG_CIDADE_NAO_ENCONTRADA = "Nao existe cadastro de cidade com código %d";
-	private static final String MSG_ESTADO_NAO_ENCONTRADO = "Nao existe cadastro de estado com código %d";
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
 
 	@Autowired
-	private EstadoRepository estadoRepository;
+	private CadastroEstadoService cadastroEstado;
 
 	public Cidade salvar(Cidade cidade) {
 		Long estadoId = cidade.getEstado().getId();
-		
-		Estado estado = estadoRepository.findById(estadoId)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException(
-						String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
-		
+		Estado estado = cadastroEstado.buscarOuFalhar(estadoId);
 		cidade.setEstado(estado);
 		
 		return cidadeRepository.save(cidade);
