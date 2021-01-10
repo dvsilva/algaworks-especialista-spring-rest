72ffedf114a7e2f76a823636f520f0fcb55f57e1
dvsilva <dvsilva@gmail.com>
2021-01-10 17:47:05 -0300

    Criando classes de mixin para usar as anotações do Jackson

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -27,9 +27,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.algaworks.algafood.core.validation.Groups;
-import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -37,7 +34,6 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@ValorZeroIncluiDescricao(valorField = "taxaFrete", descricaoField = "nome", descricaoObrigatoria = "Frete Grátis")
 public class Restaurante {
 
 	@EqualsAndHashCode.Include
@@ -45,54 +41,40 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-//	@NotNull // nao pode ser null
-//	@NotEmpty // nao pode ser vazio
-	@NotBlank // nao pode ter espacos em branco
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
-	@NotNull 
-//	@DecimalMin("0")
+	@NotNull
 	@PositiveOrZero
-//	@TaxaFrete
-//	@Multiplo(numero = 5)
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
-
-	// ignora propriedade apenas da desserialização (json de entrada)
-	@JsonIgnoreProperties(value = "nome", allowGetters = true)
+	
 	@Valid
-	@NotNull
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
-//	@JsonIgnoreProperties("hibernateLazyInitializer")
-//	@JsonIgnore
-	@ManyToOne // (fetch = FetchType.LAZY)
+	@NotNull
+	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
-
-	@JsonIgnore
+	
 	@Embedded
 	private Endereco endereco;
-
-	@JsonIgnore
+	
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataCadastro;
-
-	@JsonIgnore
+	
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
 	
-	@JsonIgnore
-	@ManyToMany // (fetch = FetchType.EAGER)
-	@JoinTable(name = "restaurante_forma_pagamento", 
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
 			joinColumns = @JoinColumn(name = "restaurante_id"),
-			inverseJoinColumns =  @JoinColumn(name = "forma_pagamento_id"))
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
 	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 	
-	@JsonIgnore
 	@OneToMany(mappedBy = "restaurante")
-	private List<Produto> produtos;
+	private List<Produto> produtos = new ArrayList<>();
 	
 }
