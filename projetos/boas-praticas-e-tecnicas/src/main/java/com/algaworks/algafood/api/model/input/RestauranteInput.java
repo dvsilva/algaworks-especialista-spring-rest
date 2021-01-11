7d04bc73a61487b40df05cdd0ac9336602f0c9e3
Danyllo <danyllo.dvs@gmail.com>
2021-01-11 09:14:42 -0300

    create project for section 11

---
+++ projetos/boas-praticas-e-tecnicas/src/main/java/com/algaworks/algafood/api/model/input/RestauranteInput.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.api.model.input;
+
+import java.math.BigDecimal;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RestauranteInput {
+	
+//	@NotNull // nao pode ser null
+//	@NotEmpty // nao pode ser vazio
+	@NotBlank // nao pode ter espacos em branco
+	private String nome;
+	
+//	@DecimalMin("0")
+//	@TaxaFrete
+//	@Multiplo(numero = 5)
+	@NotNull 
+	@PositiveOrZero
+	private BigDecimal taxaFrete;
+
+//	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
+	@Valid
+	@NotNull
+	private CozinhaIdInput cozinha;
+	
+}
