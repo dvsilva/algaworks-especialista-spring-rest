b28d4e89fa1a7f48240d24ca59178d31aff846a0
Danyllo <danyllo.dvs@gmail.com>
2021-01-11 09:00:04 -0300

    Desafio: usando DTOs como representation model

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
 
@@ -28,7 +27,7 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank
+//	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
