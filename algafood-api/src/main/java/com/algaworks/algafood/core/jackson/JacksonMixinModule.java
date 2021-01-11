6ff3ad90b56054362db7d26455d9d9824ddf1552
Danyllo <danyllo.dvs@gmail.com>
2021-01-11 07:52:14 -0300

    Refatorando e criando um assembler de DTO

--- algafood-api/src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -16,7 +16,7 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+//		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
 	}
