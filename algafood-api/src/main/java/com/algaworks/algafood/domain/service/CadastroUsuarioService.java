fa36777a613b190b4a9a2232b365783ce2f9cffe
Danyllo <danyllo.dvs@gmail.com>
2021-01-14 07:33:16 -0300

    Implementando regra de negócio para evitar usuários com e-mails duplicados

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,17 @@ public class CadastroUsuarioService {
 
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
+		// antes da consulta findByEmail o jpa sincronizaria com o banco de dados resultando em erro (mais de um mesmo e-mail cadastrado)
+		// se desanexar a entidade o jpa para de sincronizar antes da consulta
+		usuarioRepository.detach(usuario);
+		
+		Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+		
+		if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+			throw new NegocioException(
+					String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
+		}
+		
 		return usuarioRepository.save(usuario);
 	}
 
