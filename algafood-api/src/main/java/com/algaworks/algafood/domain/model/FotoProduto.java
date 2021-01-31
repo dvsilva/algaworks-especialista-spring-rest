dbada840cd72a3b2e534238d784fe78945ceae51
Danyllo <danyllo.dvs@gmail.com>
2021-01-31 06:48:51 -0300

    Excluindo e substituindo cadastro de foto de produto

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
@@ -19,14 +19,22 @@ public class FotoProduto {
 	@Id
 	@Column(name = "produto_id")
 	private Long id;
-	
+
 	@OneToOne(fetch = FetchType.LAZY)
 	@MapsId
 	private Produto produto;
-	
+
 	private String nomeArquivo;
 	private String descricao;
 	private String contentType;
 	private Long tamanho;
-	
+
+	public Long getRestauranteId() {
+		if (getProduto() != null) {
+			return getProduto().getRestaurante().getId();
+		}
+
+		return null;
+	}
+
 }
