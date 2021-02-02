083be4a646c15b5f8f04210f115bf2e956d3cd01
Danyllo <danyllo.dvs@gmail.com>
2021-02-02 08:17:51 -0300

    Selecionando a implementação do serviço de storage de fotos

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -9,7 +9,6 @@ import org.springframework.util.FileCopyUtils;
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 
-//@Service
 public class LocalFotoStorageService implements FotoStorageService {
 
 //	@Value("${algafood.storage.local.diretorio-fotos}")
