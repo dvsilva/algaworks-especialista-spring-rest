9633bec17254931545ac7954d73219a2244b029b
Danyllo <danyllo.dvs@gmail.com>
2021-03-14 09:34:41 -0300

    Desafio: adicionando hypermedia nos recursos de usuários

--- algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.api.openapi.controller;
 
-import java.util.List;
+import org.springframework.hateoas.CollectionModel;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.UsuarioModel;
@@ -18,10 +18,10 @@ public interface RestauranteUsuarioResponsavelControllerOpenApi {
     @ApiResponses({
         @ApiResponse(code = 404, message = "Restaurante não encontrado", response = Problem.class)
     })
-    List<UsuarioModel> listar(
+    CollectionModel<UsuarioModel> listar(
             @ApiParam(value = "ID do restaurante", example = "1", required = true)
             Long restauranteId);
-
+    
     @ApiOperation("Desassociação de restaurante com usuário responsável")
     @ApiResponses({
         @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
