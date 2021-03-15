2bdf2bcd929da9ea56b4da8599e47d76a8a3e831
Danyllo <danyllo.dvs@gmail.com>
2021-03-15 08:14:07 -0300

    Adicionando links para desassociação de formas de pagamento com restaurante

--- algafood-api/src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -82,6 +82,12 @@ public class AlgaLinks {
 		return linkToRestauranteFormasPagamento(restauranteId, IanaLinkRelations.SELF.value());
 	}
 	
+	public Link linkToRestauranteFormasPagamentoDesassociacao(
+			Long restauranteId, Long formaPagamentoId, String rel) {
+		return linkTo(methodOn(RestauranteFormaPagamentoController.class)
+				.desassociar(restauranteId, formaPagamentoId)).withRel(rel);
+	}
+	
 	public Link linkToRestauranteAbertura(Long restauranteId, String rel) {
 	    return linkTo(methodOn(RestauranteController.class)
 	            .abrir(restauranteId)).withRel(rel);
