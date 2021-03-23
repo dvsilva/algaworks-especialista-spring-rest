529279d41a3ef2f4c9c2d54ab2baaeccdc87ffdc
Danyllo <danyllo.dvs@gmail.com>
2021-03-23 07:54:31 -0300

    Gerando JWT com chave simétrica (HMAC SHA-256) no Authorization Server

--- projetos/stateful-authentication/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ projetos/stateful-authentication/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,8 +3,8 @@ package com.algaworks.algafood.auth;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,7 +15,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -29,6 +30,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -81,17 +85,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			// configura para não reutilizar refresh_tokens
 			// sempre que é gerado um novo tem a validade renovada (30 dias)
 			.reuseRefreshTokens(false)
-			// configurar access token converter
-			.accessTokenConverter(jwtAccessTokenConverter())
+			// configurar o redis para armazenar os tokens
+			.tokenStore(redisTokenStore())
 			.tokenGranter(tokenGranter(endpoints));
 	}
 	
-	@Bean
-	public JwtAccessTokenConverter jwtAccessTokenConverter() {
-		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey("algaworks");
-		
-		return jwtAccessTokenConverter;
+	private TokenStore redisTokenStore() {
+		return new RedisTokenStore(redisConnectionFactory);
 	}
 	
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
