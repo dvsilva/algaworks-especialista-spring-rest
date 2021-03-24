77e8ef4c76f20682aee3e43e075d71fc4b7fa66e
Danyllo <danyllo.dvs@gmail.com>
2021-03-24 08:11:13 -0300

    Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.core.security;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,14 +20,37 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 //				.antMatchers("/v1/cozinhas/**").permitAll()
-				.anyRequest().authenticated()
+//				.anyRequest().authenticated()
+				.antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
+				.anyRequest().denyAll()
 			.and()
 				.cors() // configura para permitir OPTIONS (verbos permitidos) pois browser faz pre-flyght
 			.and()
-				.oauth2ResourceServer().jwt();
 //				.oauth2ResourceServer().opaqueToken(); // configuração para opaque tokens
+				.oauth2ResourceServer().jwt()
+					.jwtAuthenticationConverter(jwtAuthenticationConverter());
 	}
 
+	// configuração para fazer restrições de acessos [extração dos claims]
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			var authorities = jwt.getClaimAsStringList("authorities");
+			
+			if (authorities == null) {
+				authorities = Collections.emptyList();
+			}
+			
+			return authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
+		});
+		
+		return jwtAuthenticationConverter;
+	}
+	
 	// configuração para chave simétrica
 //	@Bean
 //	public JwtDecoder jwtDecoder() {
