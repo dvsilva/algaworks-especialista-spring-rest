9894cb4e2ec4af03fef4c4040aebc5a015512d2d
Danyllo <danyllo.dvs@gmail.com>
2021-03-26 10:59:08 -0300

    Customizando a página de OAuth2 Approval

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecurityController {
 
+	// OAuth2 - customização da página de login
 	@GetMapping("/login")
 	public String login() {
 		return "pages/login";
 	}
-	
+
+	// OAuth2 - customização da página de aprovação (WhitelabelApprovalEndpoint)
+	@GetMapping("/oauth/confirm_access")
+	public String approval() {
+		return "pages/approval";
+	}
+
 }
